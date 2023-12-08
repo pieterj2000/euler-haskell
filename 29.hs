2551b82b5fa395bcff9385b6444c4892fb45f19b
pieterj2000 <19341833+pieterj2000@users.noreply.github.com>
2023-12-08 23:11:11 +0100

    29

---
+++ 29.hs
@@ -0,0 +1,22 @@
+import Data.List (nub)
+
+
+main = print calc
+
+exponentsUnder100 :: Int -> [Int]
+exponentsUnder100 a = [1..bound]
+    where bound = floor $ logBase aF 100
+          aF = fromIntegral a :: Double
+
+powersUnder100 :: Int -> [Int]
+powersUnder100 a = map (a^) [1..bound]
+    where bound = floor $ logBase aF 100
+          aF = fromIntegral a :: Double
+
+
+calcFor :: Int -> Int
+calcFor = length . nub . concatMap (\b -> map (*b) [2..100]) . exponentsUnder100
+
+--calc :: Int
+calc = sum . map calcFor . filter (`notElem` higherPowers) $ [2..100]
+    where higherPowers = concatMap (tail . powersUnder100) [2..100]
