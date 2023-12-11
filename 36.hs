f336f4c45e88fa798ccd703937e7818bf48b5268
pieterj2000 <19341833+pieterj2000@users.noreply.github.com>
2023-12-11 23:35:53 +0100

    kleine opti

--- 36.hs
+++ 36.hs
@@ -10,5 +10,6 @@ calc = sum $ filter (isPalindromeBase 2) $ filter (isPalindromeBase 10) $ possib
 isPalindromeBase :: Int -> Int -> Bool
 isPalindromeBase b n = let ds = intToDigitsRevBase b n in ds == reverse ds
 
+-- kan véél beter door ze expliciet te construeren, maar geen zin in gehad
 possibilities :: [Int]
-possibilities = filter (not . divisor 10) $ filter odd [1..999999]
+possibilities = filter odd [1..999999]
