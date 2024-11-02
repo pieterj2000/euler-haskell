ee5ad64792d4fc2ab407b55f5a0f4c5d222b8c4f
pieterj2000 <19341833+pieterj2000@users.noreply.github.com>
2024-11-02 22:12:15 +0100

    65

---
+++ 65.hs
@@ -0,0 +1,12 @@
+import Data.Ratio
+import Digits
+
+fraction = 2 : let doe n = 1 : (2*n) : 1 : doe (n+1) in doe 1
+
+
+calc :: [Integer] -> Rational
+calc [x] = 1 % x
+calc (x:xs) = (x % 1) + 1 / (calc xs)
+
+
+digitsum n = sum $ intToDigitsRev $ numerator $ calc $ take n fraction
